1be99968c4197c88043057a9a97cfe8e5b305c0e
Breno R Monteiro <monteiro1breno@gmail.com>
2016-11-30 20:03:32 -0300

    Last Exercise ;)

--- app-web/index.js
+++ app-web/index.js
@@ -6,6 +6,8 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(bodyParser.json());
+
 app.post("/dosave", (req,res) => {
   console.log("os dados recebidos do formulário são:");
   console.log(req.body);
@@ -15,7 +17,14 @@ app.post("/dosave", (req,res) => {
 app.post("/adivinha", (req,res) => {
   console.log("O número recebido foi:");
   console.log(req.body);
-  res.send("<h1>Será que acertou?</h1><a href='numero.html'>voltar</a>");
+  if (req.body.numero == 20 && req.body ) res.send("<h1>Acertô Mizeravi!</h1><a href='numero.html'>voltar</a>");
+  else res.send("<h1>ERRÔÔÔU</h1><a href='numero.html'>voltar</a>");
+  
+});
+
+app.get("/convidados", (req,res) => {
+  var convs = [{nomeconvidado:"Maria"},{nomeconvidado:"João"}];
+  res.send(convs);
 });
 
 app.listen(3000);
